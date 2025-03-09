2398df40b54df668ca72e09c042054f57608e7d0
awphi <26072111+awphi@users.noreply.github.com>
2025-03-09 20:12:49 +0000

    enable typechecking via pylance

--- api/bus_info.py
+++ api/bus_info.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from pydantic import BaseModel
 
-from env import get_env
+from utils import get_env
 
 
 class BusInfo(BaseModel):
@@ -13,21 +13,28 @@ class BusInfo(BaseModel):
     dest: str
 
 
+def unwrap[T](v: T | None) -> T:
+    assert v is not None
+    return v
+
+
 def fetch_bus_info() -> List[BusInfo]:
     url: str = (
         "https://www.cambridgeshirebus.info/Popup_Content/WebDisplay/WebDisplay.aspx?stopRef="
         + get_env("BUS_STOP_REF")
     )
+
     r = requests.get(url)
     result: List[BusInfo] = []
 
     if r.ok:
         soup = BeautifulSoup(r.text, features="html.parser")
-        table = soup.select_one(".rtiTable")
+        # unsafe unwrapping of Optional values but fine for now
+        table = unwrap(soup.select_one(".rtiTable"))
         for row in table.select(".gridRow"):
-            service = row.select_one(".gridServiceItem").get_text()
-            dest = row.select_one(".gridDestinationItem").get_text()
-            eta = row.select_one(".gridTimeItem").get_text()
+            service = unwrap(row.select_one(".gridServiceItem")).get_text()
+            dest = unwrap(row.select_one(".gridDestinationItem")).get_text()
+            eta = unwrap(row.select_one(".gridTimeItem")).get_text()
             result.append(BusInfo(eta=eta, service=service, dest=dest))
 
     return result
--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import asyncio
 from contextlib import asynccontextmanager
-from typing import Any, List
+from typing import Any
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
@@ -11,8 +11,8 @@ from utils import now
 
 class TransitInfo(BaseModel):
     time: int = Field(default_factory=lambda: now())
-    bus_info: List[BusInfo] = Field(default=[])
-    train_info: List[Any] = Field(default=[])
+    bus_info: list[BusInfo] = Field(default=[])
+    train_info: list[Any] = Field(default=[])
 
 
 transit_info = TransitInfo()
